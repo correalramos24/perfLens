5feb0dfeadeb319ea840f578c63c3be45a1989fb
Victor Correal <correal.ramos@gmail.com>
2025-10-29 12:22:14 +0100

    add pidstat parser@

--- perfLens/__init__.py
+++ perfLens/__init__.py
@@ -4,4 +4,5 @@
 from perfLens.parsers.AbstractParser import AbstractParser
 from perfLens.parsers.EnvParser import EnvParser
 from perfLens.parsers.MemParser import MemParser
+from perfLens.parsers.PidstatParser import PidstatParser
 from perfLens.parsers.NEMO5 import NEMO5
